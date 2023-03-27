d4d11ee23d5bb16efef11df50664b9aa79be65b1
Mritunjay Tiwari <mritunjay.tiwari@indiamart.com>
2023-03-27 20:53:50 +0530

    Working with fmt library

--- Hello Go !/main.go
+++ Hello Go !/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
-fmt.Println("Hello Go !")
+func main() {
+	fmt.Println("Hello Go !")
+
+	var myName = "Robert"
+	fmt.Println("My name is :", myName)
+
+	firstName, lastName := "Robert", "Bosch"
+
+	fmt.Println("Hey there ! I am ", firstName, " ", lastName)
 }
