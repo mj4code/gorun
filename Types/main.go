d7227ff1cf4033509f97d2b06a509fa628a72a27
mj4code <61969323+mj4code@users.noreply.github.com>
2023-06-10 12:49:29 +0530

    Develop to Main (#5)
    * Added go.mod file
    
    * Checking sonarcloud
    
    * Checking sonarcloud
    
    * Added types
    
    ---------
    
    Co-authored-by: Mritunjay Tiwari <mritunjay.tiwari@indiamart.com>

---
+++ Types/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var integer = 64
+	fmt.Printf("The type of integer is %T", integer)
+	fmt.Print("\n")
+	var float = 0.64
+	fmt.Printf("The type of float is %T", float)
+	fmt.Print("\n")
+	str := "str"
+	fmt.Printf("The type of str is %T", str)
+
+}
